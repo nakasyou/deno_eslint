515a5cce969802601644c50bb466cccf11d1b735
nakasyou113 <79000684+nakasyou@users.noreply.github.com>
2023-04-23 15:29:59 +0900

    Update test.ts

--- test.ts
+++ test.ts
@@ -7,23 +7,31 @@ const results = await eslint.lintFiles(["**/*.js"]);
 
 const isGitHubActions = Deno.env.get("GITHUB_ACTIONS") === "true";
 
+let errors = [0,0,0];
+
 for(const file of results){
   let is_err = false;
   for(const message of file.messages){
+    errors[message.severity]++;
+    
     if(!is_err){
       is_err = true;
-      console.log(file.filePath);
     }
-    const msg = `${message.line}:${message.column} ${message.message}`;
+    const msg = `  ${message.line}:${message.column} ${["","warning","error"][message.severity]} ${message.message} ${message.ruleId}`;
+    
     if(isGitHubActions){
-      core[["","warning","error"][message.severity]]("    "+msg, {
+      core[["","warning","error"][message.severity]](msg, {
         file: file.filePath,
         line: message.line,
         column: message.column,
         startLine: message.line,
       });
     }else{
-      console[["","warn","error"][message.severity]]("    "+["","Warning","Error"][message.severity]+msg);
+      console[["","warn","error"][message.severity]](msg);
     }
   }
+}
+
+if(errors[2]!==0){
+  Deno.exit(1);
 }
